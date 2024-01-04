91441eb0e8c9c3fcd8d252b31ae7a63b2e6f9c44
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2024-01-04 14:37:27 -0500

    Remove backend static files

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with all user resources');
+  res.json(
+    {message: "GET /api/users"} );
 });
 
 module.exports = router;
