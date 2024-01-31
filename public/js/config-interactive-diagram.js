95c87c227d604501ec48d49c8808cf0852bf2c59
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2024-01-31 15:03:34 -0500

    Update cooling interactive diagram

--- public/js/config-interactive-diagram.js
+++ public/js/config-interactive-diagram.js
@@ -20,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
             if (currentState == "on") {
                 currentState = "off"
                 interactiveOverlay[2].style.backgroundImage = defaultBackground
-                console.log(defaultBackground)
                 
             } else {
                 currentState = "on"
