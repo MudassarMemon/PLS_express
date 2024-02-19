513da7da9356639c7e15cb281f48c7a61c26a928
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2024-02-19 17:50:03 -0500

    Remove unnecessary scripts for commercial html

--- public/js/config-commercial.js
+++ public/js/config-commercial.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     ytButtons1 = document.querySelector(".ytp-chrome-top")
     ytButtons2 = document.querySelector(".ytp-show-cards-title")
-
     console.log(ytButtons1)
     console.log(ytButtons2)
 })
